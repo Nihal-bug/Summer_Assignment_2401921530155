79cc8b512a8d5893660b13dcee8dc6b6840f8e31
Nihal-bug <guptanihal2406@gmail.com>
2026-06-02 23:04:42 +0530

    Create containsduplicate.java

---
+++ Week_1/Day_2/containsduplicate.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> values = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (values.contains(nums[i])) {
+                return true;
+            }
+            values.add(nums[i]);
+        }
+
+        return false;
+        
+    }
+}
