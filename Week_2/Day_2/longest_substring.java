22e4a856a443e9ff6150cf6a471430cbc2b2e137
Nihal-bug <guptanihal2406@gmail.com>
2026-06-09 22:56:09 +0530

    Create longest_substring.java

---
+++ Week_2/Day_2/longest_substring.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        HashSet<Character> set = new HashSet<>();
+
+        int l = 0;
+        int maxLen = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+
+            while (set.contains(s.charAt(r))) {
+                set.remove(s.charAt(l));
+                l++;
+            }
+
+            set.add(s.charAt(r));
+            maxLen = Math.max(maxLen, r - l + 1);
+        }
+
+        return maxLen;
+    }
+}
